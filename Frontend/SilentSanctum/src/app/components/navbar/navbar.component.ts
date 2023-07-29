4b06de9e48a1cdbd759af248e550d47eddcb56a0
Prajwal Patil <prajwalpatilk@gmail.com>
2023-07-29 21:56:11 +0530

    Add logout button

--- Frontend/SilentSanctum/src/app/components/navbar/navbar.component.ts
+++ Frontend/SilentSanctum/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  constructor(
+    @Inject(DOCUMENT) public document: Document,
+    public auth: AuthService
+  ) {}
 
+  logoutUser() {}
 }

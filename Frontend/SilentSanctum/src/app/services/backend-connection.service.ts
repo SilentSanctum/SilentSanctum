97fcdb032fd6dd2a59877703441eb425e38f2f7e
Rondy <ganesh8497993502@gmail.com>
2023-07-29 17:31:22 +0530

    backend services

---
+++ Frontend/SilentSanctum/src/app/services/backend-connection.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendConnectionService {
+
+  private baseUrl = 'http://localhost:3000';
+  constructor(private http: HttpClient) {}
+
+  login(profile: any) {
+    const body = profile;
+    return this.http.post<any>(`${this.baseUrl}/login`, body);
+  }
+}

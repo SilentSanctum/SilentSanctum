52269bda5855c1732a5797e3a8949962572a66d8
Prajwal Patil <prajwalpatilk@gmail.com>
2023-07-29 19:39:19 +0530

    Update style

--- Frontend/SilentSanctum/src/app/app.module.ts
+++ Frontend/SilentSanctum/src/app/app.module.ts
@@ -7,26 +7,31 @@ import { AuthModule } from '@auth0/auth0-angular';
 import { environment } from 'src/environments/envronmet';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     // AuthModule.forRoot(environment.auth),
     AuthModule.forRoot({
       domain: 'dev-yhlqy75o81n2y7y6.us.auth0.com',
       clientId: 'f4mPTWqyTXtMb9FkDT8R490Dwsr9TUtA',
       authorizationParams: {
-        redirect_uri: 'http://localhost:4200/home'
-      }
+        redirect_uri: 'http://localhost:4200/home',
+      },
     }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- Frontend/SilentSanctum/src/app/components/home/home.component.ts
+++ Frontend/SilentSanctum/src/app/components/home/home.component.ts
@@ -1,12 +1,37 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { BackendConnectionService } from 'src/app/services/backend-connection.service';
+import { User } from '@auth0/auth0-spa-js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
+export class HomeComponent implements OnInit {
+  profileJson: any;
+  constructor(
+    @Inject(DOCUMENT) public document: Document,
+    public auth: AuthService,
+    public backendService: BackendConnectionService
+  ) {}
+
+  ngOnInit(): void {
+    // this.auth.user$.subscribe(
+    //   (profile) =>{
+    //     console.log("profile from subs: ", profile);
+    //     (this.profileJson = profile);
+    //   },
+    // );
+    // console.log("profile data: ", this.profileJson);
+    this.auth.user$.subscribe((profile) => {
+      // console.log("profile from subs login:", profile);
+      this.profileJson = profile;
+      console.log('profile json:', this.profileJson);
+      this.backendService.login(profile).subscribe((response) => {
+        console.log('response: ', response);
+      });
+    });
+  }
 }

0ce0d5003464eddec01f01b3b88472c3d88611a7
Rondy <ganesh8497993502@gmail.com>
2023-07-29 23:05:28 +0530

    login screen alignment

--- Frontend/SilentSanctum/src/app/components/navbar/navbar.component.ts
+++ Frontend/SilentSanctum/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  profileJson: any;
+  userProfilePic: any;
 
+  constructor( public auth: AuthService ) {}
+  
+  ngOnInit(): void {
+    this.auth.user$.subscribe((profile) => {
+      // console.log("profile from subs login:", profile);
+      this.profileJson = profile;
+      console.log('profile json:', this.profileJson);
+    });
+  }
 }

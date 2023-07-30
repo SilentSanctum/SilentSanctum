5097238ca4055d44fa8b74e4d668add8ca732576
Rondy <ganesh8497993502@gmail.com>
2023-07-30 11:59:57 +0530

    added dynamic time remaining functionality

--- Frontend/SilentSanctum/src/app/components/posts/posts.component.ts
+++ Frontend/SilentSanctum/src/app/components/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BackendConnectionService } from 'src/app/services/backend-connection.service';
 import { CommentsService } from 'src/app/services/comments.service';
 import { PostsService } from 'src/app/services/posts.service';
@@ -8,13 +8,14 @@ import { PostsService } from 'src/app/services/posts.service';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css'],
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit, OnDestroy {
   constructor(
     public backendService: BackendConnectionService,
     private postService: PostsService,
     private commentsService: CommentsService
   ) {}
   allPosts: any = null;
+  private updateInterval: any;
 
   calculateRemainingTime(created: any) {
     const createdTimestamp = Date.parse(created);
@@ -24,6 +25,7 @@ export class PostsComponent {
       return {
         hours: 0,
         minutes: 0,
+        seconds: 0,
       };
     }
 
@@ -32,19 +34,37 @@ export class PostsComponent {
     const minutesRemaining = Math.floor(
       (timeRemaining % (60 * 60 * 1000)) / (60 * 1000)
     );
+    const secondsRemaining = Math.floor((timeRemaining % (60 * 1000)) / 1000);
     return {
       hours: hoursRemaining,
       minutes: minutesRemaining,
+      seconds: secondsRemaining,  
     };
   }
 
   displayRemainingTime(timeString: any) {
     let rem = this.calculateRemainingTime(timeString);
+    // if (rem.hours > 0) {
+    //   return String(rem.hours) + ' hrs';
+    // } else {
+    //   return String(rem.minutes) + ' mins';
+    // }
     if (rem.hours > 0) {
-      return String(rem.hours) + ' hrs';
-    } else {
-      return String(rem.minutes) + ' mins';
+      return `${rem.hours} hrs ${rem.minutes} mins ${rem.seconds} secs`;
+    } else if (rem.minutes > 0) {
+      return `${rem.minutes} mins ${rem.seconds} secs`;
+    } 
+    else {
+      return `${rem.seconds} secs`;
     }
+  }
+
+  updateRemainingTime() {
+    // const now = new Date();
+    this.allPosts.forEach((post: any) => {
+      const remainingTime = this.calculateRemainingTime(post.createdAt);
+      post.remainingTime = this.displayRemainingTime(remainingTime);
+    });
   }
 
   getComments(commentId: any) {
@@ -54,5 +74,15 @@ export class PostsComponent {
   ngOnInit() {
     this.postService.changePosts();
     this.postService.postsFetched.subscribe((posts) => (this.allPosts = posts));
+    this.updateRemainingTime();
+    this.updateInterval = setInterval(() => {
+      this.updateRemainingTime(); // Update every second (adjust as needed)
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    if (this.updateInterval) {
+      clearInterval(this.updateInterval); // Clear the interval when the component is destroyed
+    }
   }
 }
